55719e2b832514f90eac35db2cf171fa7f7b2ce0
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-02-09 17:48:27 +0530

    Update 54.c

--- 54.c
+++ 54.c
@@ -2,16 +2,16 @@
  
 int main(void) 
 {
-	int num,even;
+	int n,even;
 	printf("enter the number:");
-	scanf("%d",&num);
-	if(num%2==0)
+	scanf("%d",&n);
+	if(n%2==0)
 	{
-		even=num-2;	
+		even=n-2;	
 	}
 	else
 	{
-		even=num-1;
+		even=n-1;
 	}
 	printf("\n%d",even);
 	return 0;
